48bd40c75c97962ce91306034cd04eb2c9cf33ef
KimitaSora <kamaaya1118@gmail.com>
2019-10-18 16:30:04 +0900

    ファイルとかのひな形

---
+++ HarfToyParty/Assets/Scripts/Gimmick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GimmickKind{
+    Bomb,
+    MoveWall,
+    Warp,
+}
+
+public class Gimmick : MonoBehaviour
+{
+    
+}
