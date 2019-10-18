9205fc60970fa6ad109df22319e46c5804d33ca9
Nerlite <ctrlaltenter0@gmail.com>
2019-10-18 22:22:44 +0900

    タイトル画面とキャラ確認画面途中まで

---
+++ HarfToyParty/Assets/Scripts/Title.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+    
+    void Start () {
+
+    }
+
+    
+    void Update () {
+        if (Input.anyKey) {
+            SceneManager.LoadScene("PlayerPrewiewScene");
+        }
+    }
+}
