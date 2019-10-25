66528889c5f18c7dc3466186b1e7ff21d5e5d62e
ryusuke <ryusuke0428tora@gmail.com>
2019-10-25 16:58:58 +0900

    Assets直下にFujitaフォルダを追加 ギミックテスト用のscene追加 ボム用のscript追加

---
+++ HarfToyParty/Assets/Fujita/BomTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomTest : MonoBehaviour
+{
+    private GameObject Bomb;
+    [SerializeField]
+    private float explosionTime;
+    private float rimeTrigger;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //private void explosion()
+    //{
+    //   ExplosionCoroutin();
+    //}
+
+    //IEnumerator ExplosionCoroutin()
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(explosionTime);
+    //    }
+    //}
+}
