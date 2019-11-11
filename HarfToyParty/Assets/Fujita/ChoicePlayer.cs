a247ec49b3efe4036f2a17cc9a2a21878876dd79
muxu1118 <kamaaya1118@gmail.com>
2019-11-11 15:41:35 +0900

    チョイス部分作成開始

---
+++ HarfToyParty/Assets/Fujita/ChoicePlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChoicePlayer : MonoBehaviour{
+
+    public GameObject Player1, Player2;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void choice()
+    {
+        if (Application.isEditor)
+        {
+            //デバック用
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("プレイヤーを選択した");
+                if (Player1)
+                {
+                    Debug.Log("1を選択した");
+                    //背景を1に変える
+                }
+                if (Player2)
+                {
+                    Debug.Log("2を選択した");
+                    //背景を2に変える
+                }
+            }
+            //タブレット用
+            else if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    Debug.Log("タップした");
+                }
+
+            }
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
