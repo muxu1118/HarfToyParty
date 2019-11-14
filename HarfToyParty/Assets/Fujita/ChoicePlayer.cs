5d23b6311ecefca8862334e412c4c7a63581127a
fujita <>
2019-11-14 15:29:07 +0900

    端末に対してPlayerを与える(途中)

--- HarfToyParty/Assets/Fujita/ChoicePlayer.cs
+++ HarfToyParty/Assets/Fujita/ChoicePlayer.cs
@@ -10,6 +10,11 @@ public class ChoicePlayer : MonoBehaviour{
     public static int Pl_1score = 0;
     public static int Pl_2score = 0;
 
+    public MapKind playerID;//Player1か2
+
+    public int[] player = new int[2];
+    
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -22,13 +27,15 @@ public class ChoicePlayer : MonoBehaviour{
         //デバック用
             if (Player1)
             {
+                
                 Debug.Log("1を選択した");
-                //背景をキャラ1に変える＋Pl_1scoreを持たせる
-
+                playerID = MapKind.Player1;
+                //Pl_1scoreを持たせる
             }
             else if (Player2)
             {
                 Debug.Log("2を選択した");
+                playerID = MapKind.Player2;
             }
 
 
