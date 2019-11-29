023ee05252449bc40e76862702bdb6aa74c0013c
ryusuke <ryusuke0428tora@gmail.com>
2019-11-29 19:24:16 +0900

    アルファ版に向けてTitleのScriptを追加+少し変更をかけた(横画面)

---
+++ HarfToyParty/Assets/Fujita/Title_a.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_a : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //taptostartをフワフワさせる
+
+    //タイトルロゴを上にあげる
+    //キャラをどうするか
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
