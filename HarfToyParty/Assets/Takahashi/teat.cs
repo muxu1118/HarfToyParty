904da882527803ae4a72f6d5d5b5986afc07eab5
natuto1998 <kouta.taka.0811.15@gmail.com>
2020-01-16 02:04:04 +0900

    リザルトUIの仮置きと表示の仕方を変更

--- HarfToyParty/Assets/Takahashi/teat.cs
+++ HarfToyParty/Assets/Takahashi/teat.cs
@@ -6,17 +6,39 @@ using UnityEngine.UI;
 public class teat : MonoBehaviour
 {
     [SerializeField]
-    Text text;
+    //Image[] image;
+    Sprite[] image;
+    int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        text.text = "koko";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PartGet()
+    {
+        Debug.Log("hazime");
+        i++;
+        Debug.Log(i);
+        switch (i)
+        {
+            case 1:
+                image[0] = Resources.Load<Sprite>("Sprites/Part/gimmick_body3_B");
+                //Debug.LogError("来た");
+                break;
+            case 2:
+                image[1]= Resources.Load<Sprite>("Sprites/Part/gimmick_body01_B");
+                break;
+            case 3:
+                image[2]= Resources.Load<Sprite>("Sprites/Part/gimmick_body2_B");
+                break;
+        }
     }
 }
