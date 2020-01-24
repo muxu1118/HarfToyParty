0456bb046775d566588a79ab14241e51d045f61e
natuto1998 <kouta.taka.0811.15@gmail.com>
2020-01-24 14:49:02 +0900

    リザルトUIの演出を変更

--- HarfToyParty/Assets/Takahashi/WinLoss.cs
+++ HarfToyParty/Assets/Takahashi/WinLoss.cs
@@ -12,26 +12,70 @@ public class WinLoss : MonoBehaviour
     Image red;
     [SerializeField]
     Image blue;
-    //[SerializeField]    
-    //GameObject[] player;
-
+   
     Sprite Win; //Winのスプライト画像を取得するためのもの
     Sprite Lose; //Loseのスプライト画像を取得するためのもの
-
-    //[SerializeField]
-    //GameObject[] crownTear;
-
+    
     [SerializeField]
     GameObject[] Red_resultUI;
     [SerializeField]
     GameObject[] Blue_resultUI;
 
+    [SerializeField]
+    float b;
+    //王冠の場合b = 0.2が弟　-0.2が兄
+
+    GameObject crownPrefab;
+    GameObject tearPrefab;
+
+    bool slore = true;       
+
+    private void Update()
+    {
+        //位置確認用
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Quaternion rote = new Quaternion(0.0f, 0.0f, b, 1.0f);
+            
+            GameObject crown = Instantiate(Red_resultUI[0], new Vector3(1.0f, 2.0f, 0.0f), rote);
+            crown.transform.parent = panel.transform;            
+        }
+    }
+
     void Start()
-    {                
+    {        
         //1P勝利の画像を取得
         Win = Resources.Load<Sprite>("Sprites/WinLose/w");
         //2P勝利の画像を取得
         Lose = Resources.Load<Sprite>("Sprites/WinLose/l");
+        crownPrefab = (GameObject)Resources.Load("Prefabs/crown");
+        tearPrefab = (GameObject)Resources.Load("Prefabs/tear");
+    }
+
+    /// <summary>
+    /// 王冠を生成する
+    /// </summary>
+    private void crownGenerate()
+    {
+        //赤が勝利の場合
+        if (slore)
+        {             
+            b *= -0.1f;
+        }        
+        Quaternion rote = new Quaternion(0.0f, 0.0f, b, 1.0f);
+        //
+        GameObject crowCroaw = Instantiate(crownPrefab, new Vector3(1.0f, 2.0f, 0.0f), rote);
+        crowCroaw.transform.parent = panel.transform;
+    }
+
+    /// <summary>
+    /// 涙を生成する
+    /// </summary>
+    private void tearGenerate()
+    {
+        //赤が敗北の場合        
+        GameObject crowTear = Instantiate(tearPrefab, new Vector3(1.0f, 2.0f, 0.0f), Quaternion.identity);
+        crowTear.transform.parent = panel.transform;
     }
 
     /// <summary>
@@ -46,27 +90,22 @@ public class WinLoss : MonoBehaviour
         switch (winnerDesplay)
         {
             case 1:
-                //赤の勝利
+                
                 blue.sprite = Lose;
 
-                //crownTear[0].SetActive(true);
-                //crownTear[1].SetActive(false);
                 Blue_resultUI[0].SetActive(false);
                 Blue_resultUI[1].SetActive(false);
 
-                //player[1].SetActive(false);
                 Red_resultUI[2].SetActive(false);
                 break;
             case 2:
-                //青の勝利                
+                //青の勝利  
+                slore = false;
                 red.sprite = Lose;
 
-                //crownTear[0].SetActive(false);
-                //crownTear[1].SetActive(true);
                 Red_resultUI[0].SetActive(false);
                 Red_resultUI[1].SetActive(false);
 
-                //player[0].SetActive(false);
                 Blue_resultUI[2].SetActive(false);
                 break;
         }        
---
+++ HarfToyParty/Assets/Takahashi/titl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titl : MonoBehaviour
+{   
+    private void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// タイトルシーンに移動
+    /// </summary>
+    public void Onclick()
+    {
+        GameManager.instance.StateChange();
+        SceneManager.LoadScene("Choice");        
+        this.gameObject.SetActive(false);
+    }
+
+    private void gomi()
+    {
+        //    [SerializeField]
+        //string[] serihu;
+        //string ko;
+
+        //int[] mozisuu = new int[3];
+
+        //private void Start()
+        //{
+        //    for (int i = 0; i < serihu.Length; i++)
+        //    {
+        //        ko = serihu[i];
+        //        mozisuu[i] = ko.Length;
+        //        Debug.Log(mozisuu[i]);
+        //    }
+        //    //ko = serihu[0];
+        ////serihu[0] = "ko";
+        //mozisuu[0] = ko.Length;
+        //Debug.Log(mozisuu[0]);
+    }
+}      
