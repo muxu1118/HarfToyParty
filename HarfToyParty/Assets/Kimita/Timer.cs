d38a06a3b7ebae02a2cb6b6e95f023cdac126984
kimitasora <kamaaya1118@gmail.com>
2020-02-07 20:10:19 +0900

    BreakBlockの色変え Timerの作成

--- HarfToyParty/Assets/Kimita/Timer.cs
+++ HarfToyParty/Assets/Kimita/Timer.cs
@@ -8,5 +8,36 @@ using UnityEngine.UI;
  */
 public class Timer : MonoBehaviour
 {
+    [SerializeField]
+    private float count;
+    private float timeLimit;
+    //[SerializeField]
+    private Slider timeSlider;
+
+    private void Start()
+    {
+        //timeLimit = GameManager.instance.gameRule.Time;
+        timeLimit = 300;
+        count = 0;
+        timeSlider = gameObject.GetComponent<Slider>();
+    }
+    private void Update()
+    {
+        TimeCount();
+    }
     
+    private void TimeCount()
+    {
+        if (timeLimit >= count)
+        {
+            count += Time.deltaTime;
+            // 時間をSliderのValueに入れる
+            timeSlider.value = 1-(1f/(timeLimit / count));
+        }
+        else
+        {
+            // 引き分け動作
+            Debug.Log("TimeUp");
+        }
+    }
 }
